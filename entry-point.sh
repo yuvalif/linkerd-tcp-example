2f6b843cbf925468fbce36de33fdaf04adb77791
Yuval Lifshitz <yuvalif@yahoo.com>
2018-06-07 15:01:39 +0300

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -2,8 +2,8 @@
 
 # run namerd
 mkdir -p disco
-/namerd-1.0.2-exec namerd.yml #> ./namerd.log 2>&1 &
+/namerd-1.0.2-exec namerd.yml > ./namerd.log 2>&1 &
 
 # run linkerd-tcp and block
-/linkerd-tcp linkerd-tcp.yml #> ./linkerd-tcp.log 2>&1
+/linkerd-tcp linkerd-tcp.yml > ./linkerd-tcp.log 2>&1
 
